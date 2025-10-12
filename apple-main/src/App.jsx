4ef96168b1939db808cf23fa3d25f780050a3ac9
kaushal kumar jha <jhakaushal361@gmail.com>
2025-10-13 03:23:08 +0530

    first commit

---
+++ apple-main/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+
+// import './app.css'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import ProductViewer from './components/ProductViewer'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <ProductViewer />
+
+
+    </main>
+  )
+}
+
+export default App
